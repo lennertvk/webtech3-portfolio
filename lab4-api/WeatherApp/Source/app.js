ad4e4e4fa796f90a44823b37d4508be8c0b2d1cd
Lennert Van Kerckhove <lennert.van.kerckhove@gmail.com>
2019-03-21 15:33:08 +0100

    meme api is working

--- lab4-api/WeatherApp/Source/app.js
+++ lab4-api/WeatherApp/Source/app.js
@@ -1,19 +1,18 @@
 class Weather{
     constructor(API_KEY){
         this.API_KEY = API_KEY;
-        console.log("test");
+       // console.log("test");
         this.initialize();
     }
 
 
     initialize(){
-        //console.log(navigator);
         this.getMyLocation();         
     }
 
     getMyLocation(){
         navigator.geolocation.getCurrentPosition((position) => {//hetzelfde als function (position){}
-        console.log("gevonden");   
+       // console.log("gevonden");   
         let lat = position.coords.latitude;
         let lng = position.coords.longitude;
 
@@ -24,60 +23,58 @@ class Weather{
     }
 
     getWeather(lat, lng){
-        // AJAX call / XHR
-       //// https://api.darksky.net/forecast/db4523c613cbbd173ea77a80db733bf5/37.8267,-122.4233?units=si
-       // db4523c613cbbd173ea77a80db733bf5
-       // https://cors-anywhere.herokuapp.com/
-       // https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/db4523c613cbbd173ea77a80db733bf5/37.8267,-122.4233?units=si
-
         let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}?units=si`
         fetch(url)
         .then(response => {
-            
+
             return response.json();
         })
         .then(json => {
-            console.log(json);
-           // let temp = document.createElement('h1');
-           // temp.innerHTML = json.currently.summary;
-            //let cursum = setInterval(json.currently.summary, 1000 * 60 * 60);
-            // zorg dat de de api elk uur aangesproken word
-            // laad de gegevens in de local storage
-            // print de gegeevens uit de local storage
-
-            //let cursum = setInterval(json.currently.summary, 1000 * 5);
-            //let cursum = alert('testinggg');
-            //console.log(cursum);
-
-           // localStorage.setItem('current-summary', JSON.stringify(cursum));
-
-            console.log(json.currently.summary);
+           //  console.log(json.currently.summary);
+           console.log(json);
             document.getElementById('shortdiscription').innerHTML = json.currently.summary;
             let atthisminute = json.hourly.summary;
             document.getElementById('atthisminute').innerHTML = atthisminute;
         })
     }
 }
+
 let app = new Weather('db4523c613cbbd173ea77a80db733bf5'); // zo kan je er een online plugin van maken en kunnen mensen zelf hun key ingeven
 // komt binnen in de constructor
 
-class Yoga{
-    constructor(){
+class Meme{
+    constructor(API_KEY){
+        this.API_KEY = API_KEY;
         this.initialize();
     }
 
     initialize(){
-        this.getRandomnr(); 
-     // console.log(randomnr);
+        this.getMeme();   
+     // console.log("test2");
     }
 
-    getRandomnr(){
-       let randomnr = Math.floor((Math.random() * 10) + 1);
-       console.log(randomnr);
-       this.getRandomYogaPosition(randomnr);
-    }
+    getMeme(){
+        console.log("test3");
+        let url = `http://version1.api.memegenerator.net//Instances_Search?q=coding&apiKey=${this.API_KEY}`;
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            let randomnr = Math.floor(Math.random() * (json.result.length - 2));
+            //console.log(randomnr);
+            let memeurl = json.result[randomnr]['imageUrl'];
+            document.getElementById('meme').innerHTML = '<img src="' + memeurl +'">';
 
-    getRandomYogaPosition(randomnr){
-        let url = `baseurl.com/api/${randomnr}`;
+            document.getElementById('memesearch').addEventListener('keypress', function (e) {
+                    var key = e.which || e.keyCode;
+                    if (key === 13) { //stackoverflow
+                        console.log("enter");
+                    }
+                });
+
+         })
     }
 }
+
+let meme = new Meme('2014ed7c-1554-4504-9810-2d6713d79f37');
